1ebc46be7ad8635221c7fc6be51db3c208e83304
Chester133 <monstersolider0@gmail.com>
2024-10-30 01:17:35 +0100

    Add files via upload

--- headers/PrimitiveRenderer.cpp
+++ headers/PrimitiveRenderer.cpp
@@ -1,6 +1,9 @@
 #include "PrimitiveRenderer.h"
 #include <cmath>
 
+#define PI 3.14159265358979323846
+
+
 using namespace std;
 
 PrimitiveRenderer::PrimitiveRenderer(sf::RenderWindow& window) : window(window) {}
@@ -94,4 +97,112 @@ void PrimitiveRenderer::drawOSline(const vector<LineSegment>& segments, sf::Colo
         drawLineIncremental(lastSegment.getEndPoint().getX(), lastSegment.getEndPoint().getY(),
                             firstSegment.getStartPoint().getX(), firstSegment.getStartPoint().getY(), color);
     }
+}
+
+void PrimitiveRenderer::drawCircleSym4(double radius, double x0, double y0, sf::Color color) {
+for (float i=0;i <=PI/2;i=i+(PI/180))
+   {
+
+    double x1 = x0 + radius*cos(0+i);
+    double y1 = y0 + radius*sin(0+i);
+
+    double x2 = x0 + radius*cos(PI/2+i);
+    double y2 = y0 + radius*sin(PI/2+i);
+
+    double x3 = x0 + radius*cos(PI+i);
+    double y3 = y0 + radius*sin(PI+i);
+
+    double x4 = x0 + radius*cos(PI*1.5+i);
+    double y4 = y0 + radius*sin(PI*1.5+i);
+
+        sf::Vertex line1[] = {
+            sf::Vertex(sf::Vector2f(x1, y1), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+        sf::Vertex line2[] = {
+            sf::Vertex(sf::Vector2f(x2, y2), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+        sf::Vertex line3[] = {
+            sf::Vertex(sf::Vector2f(x3, y3), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+        sf::Vertex line4[] = {
+            sf::Vertex(sf::Vector2f(x4, y4), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+
+        window.draw(line1 , 2 , sf::Lines);
+        window.draw(line2 , 2 , sf::Lines);
+        window.draw(line3 , 2 , sf::Lines);
+        window.draw(line4 , 2 , sf::Lines);
+    }
+
+    
+}
+
+void PrimitiveRenderer::drawElipseSym4(double radiusX,double radiusY, double x0, double y0, sf::Color color) {
+for (float i=0;i <=PI/2;i=i+(PI/180))
+   {
+
+    double x1 = x0 + radiusX*cos(0+i);
+    double y1 = y0 + radiusY*sin(0+i);
+
+    double x2 = x0 + radiusX*cos(PI/2+i);
+    double y2 = y0 + radiusY*sin(PI/2+i);
+
+    double x3 = x0 + radiusX*cos(PI+i);
+    double y3 = y0 + radiusY*sin(PI+i);
+
+    double x4 = x0 + radiusX*cos(PI*1.5+i);
+    double y4 = y0 + radiusY*sin(PI*1.5+i);
+
+        sf::Vertex line1[] = {
+            sf::Vertex(sf::Vector2f(x1, y1), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+        sf::Vertex line2[] = {
+            sf::Vertex(sf::Vector2f(x2, y2), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+        sf::Vertex line3[] = {
+            sf::Vertex(sf::Vector2f(x3, y3), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+        sf::Vertex line4[] = {
+            sf::Vertex(sf::Vector2f(x4, y4), color),
+            sf::Vertex(sf::Vector2f(x0, y0), color)
+        };
+
+
+        window.draw(line1 , 2 , sf::Lines);
+        window.draw(line2 , 2 , sf::Lines);
+        window.draw(line3 , 2 , sf::Lines);
+        window.draw(line4 , 2 , sf::Lines);
+    }
+
+    
+}
+
+void PrimitiveRenderer::drawPolygon(const vector<Point2D>& points, sf::Color color, bool closed) {
+    sf::VertexArray OSline(closed ? sf::LinesStrip : sf::LineStrip, points.size() + (closed ? true : false));
+
+    for(int i=0;i<points.size();++i) {
+        OSline[i].position = sf::Vector2f(points[i].getX(), points[i].getY());
+        OSline[i].color = color;
+    }
+
+    if(closed) {
+        OSline[points.size()].position = sf::Vector2f(points[0].getX(), points[0].getY());
+        OSline[points.size()].color = color;
+    }
+
+    window.draw(OSline);
 }
--- headers/PrimitiveRenderer.h
+++ headers/PrimitiveRenderer.h
@@ -13,11 +13,14 @@ public:
     PrimitiveRenderer(sf::RenderWindow& window);
 
     void drawCircle(float radius, float x, float y, sf::Color color);
+    void drawCircleSym4(double radius, double x, double y, sf::Color color);
+    void drawElipseSym4(double radiusX,double radiusY, double x, double y, sf::Color color);
     void drawRectangle(float width, float height, float x, float y, sf::Color color);
     void drawLine(float startX, float startY, float endX, float endY, sf::Color color);
     void drawLineIncremental(float x0, float y0, float x1, float y1, sf::Color color);
     void drawOSline(const std::vector<Point2D>& points, sf::Color color, bool closed = false);
     void drawOSline(const std::vector<LineSegment>& segments, sf::Color color, bool closed = false);
+    void drawPolygon(const std::vector<Point2D>& points, sf::Color color, bool closed = false);
 };
 
 #endif // PRIMITIVERENDERER_H
